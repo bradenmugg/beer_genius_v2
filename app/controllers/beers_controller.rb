282dcb0965101d9def83dc8a109bd8522948b8d9
Braden Perez-Mugg <braden.mugg@gmail.com>
2015-08-19 12:41:32 -0400

    Add api gems

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,13 @@
 class BeersController < ApplicationController
+  respond_to :json
+
   def search
-    @search = Post.search(:include => [:comments]) do
+    @search = Beer.search(:include => [:comments]) do
       keywords("Fat")
   end
+
+  def show
+    respond_with Beer.find(params[:style])
+  end
+
 end
--- test.rb
+++ test.rb
@@ -1,5 +1,16 @@
 filename = "/Users/marthaperez-mugg/Documents/start_up/brewery_app/breweries.csv"
 File.open(filename).each do |line|
   attributes = line.split(',')
-  puts attributes
+  brewery = attributes[0]
+  name = attributes[1]
+  api_id = attributes[2]
+  style = attributes[3]
+  abv = attributes[4]
+  ibu = attributes[5]
+  Beer.create!(brewery: brewery, 
+               name: name, 
+               api_id: api_id, 
+               style: style, 
+               abv: abv,
+               ibu: ibu)
 end
